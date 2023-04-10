41eca033df304d4c6a3e67c1e3d806675894c82d
Minh-Kop <87182623+Minh-Kop@users.noreply.github.com>
2023-04-10 19:27:34 +0700

    Try to create conflict

--- Project_PTTK/src/test/Main.java
+++ Project_PTTK/src/test/Main.java
@@ -2,6 +2,7 @@ package test;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello cac ban, minh la JavaKhoi!");
+		String test = "123";
+		System.out.println("My name is " + test);
 	}
 }
