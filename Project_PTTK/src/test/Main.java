5baf791b333a1002f2c7f9aa1207d973344d9ec4
Minh-Kop <87182623+Minh-Kop@users.noreply.github.com>
2023-04-10 19:08:29 +0700

    Try branch khoi again

--- Project_PTTK/src/test/Main.java
+++ Project_PTTK/src/test/Main.java
@@ -2,6 +2,6 @@ package test;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello cac ban, minh la Khoi!");
+		System.out.println("Hello cac ban, minh la Minh Khoi!");
 	}
 }
