112de57414c5b7052c165238c89feed667ccca76
acer <acer@172.29.87.181>
2023-04-12 09:39:28 +0700

    First commit in branch kiet

---
+++ Project_PTTK/src/view/HoaDonView.java
@@ -0,0 +1,47 @@
+package view;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+
+public class HoaDonView extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					HoaDonView frame = new HoaDonView();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public HoaDonView() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		
+		JLabel lblNewLabel = new JLabel("Hoa don");
+		lblNewLabel.setFont(new Font("SVNtimes new roman H", Font.PLAIN, 26));
+		contentPane.add(lblNewLabel);
+	}
+
+}
